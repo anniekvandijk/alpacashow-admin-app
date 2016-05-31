b76f3334c134039c06ad99a738de60f9b9ba73ce
Anniek van Dijk <anniek@animundo.nl>
2016-05-31 23:50:14 +0200

    frontend aan de praat (geeft geen errors, wel wat zaken nog niet goed)

--- alpacashow-admin-frontend/src/main/app/ts/app.component.ts
+++ alpacashow-admin-frontend/src/main/app/ts/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
-  template: '<h1>My First Angular 2 App</h1>'
+  template: '<h1>Hello Angular 2 World!</h1>'
 })
 export class AppComponent { }
