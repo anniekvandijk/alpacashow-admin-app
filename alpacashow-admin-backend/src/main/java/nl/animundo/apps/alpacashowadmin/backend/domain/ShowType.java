3fb43c8da8b236da9e7e522fc645a583a21c1760
Anniek van Dijk <anniek@animundo.nl>
2016-06-12 21:44:43 +0200

    Refactor ShowType

--- alpacashow-admin-backend/src/main/java/nl/animundo/apps/alpacashowadmin/backend/domain/ShowType.java
+++ alpacashow-admin-backend/src/main/java/nl/animundo/apps/alpacashowadmin/backend/domain/ShowType.java
@@ -1,21 +1,15 @@
 package nl.animundo.apps.alpacashowadmin.backend.domain;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 /**
- * Created by Anniek van Dijk on 12-6-2016.
+ * Created by Anniek van Dijk on 8-6-2016.
  */
-public class ShowType {
-    private static Logger logger = LoggerFactory.getLogger(ShowType.class);
+public enum ShowType {
 
-    private ShowTypes showTypes;
-
-    public ShowType(ShowTypes showTypes) {
-        this.showTypes = showTypes;
-    }
-
-    public ShowTypes getShowTypes() {
-        return showTypes;
-    }
+    HALTERSHOW,
+    FLEECESHOW,
+    MALEPROGENYSHOW,
+    FEMALEPROGENYSHOW;
 }
+
+
+
