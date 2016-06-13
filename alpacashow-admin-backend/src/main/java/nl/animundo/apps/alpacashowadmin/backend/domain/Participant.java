c61f6ef123a9df0943c1fcfad8b38a72988e49a8
Anniek van Dijk <anniek@animundo.nl>
2016-06-13 21:13:34 +0200

    Add Animals to Participant

--- alpacashow-admin-backend/src/main/java/nl/animundo/apps/alpacashowadmin/backend/domain/Participant.java
+++ alpacashow-admin-backend/src/main/java/nl/animundo/apps/alpacashowadmin/backend/domain/Participant.java
@@ -1,25 +1,43 @@
 package nl.animundo.apps.alpacashowadmin.backend.domain;
 
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by Anniek van Dijk on 5-6-2016.
  */
 public class Participant {
+    private static Logger logger = LoggerFactory.getLogger(Participant.class);
 
     private String name;
+    private Set<Animal> animals;
 
     public Participant(final String name) {
+        this(name, new HashSet<Animal>());
+    }
+
+    public Participant(final String name, final Set<Animal> animals) {
 
         final String nameCln = StringUtils.trimToNull(name);
         if (nameCln == null) {
             throw new IllegalArgumentException("Field participant can not be empty");
         }
+        logger.info("\n Participant: " + name + "\n Animal(s): " + animals);
         this.name = nameCln;
+        this.animals = animals;
     }
 
     public String getName() {
         return name;
     }
+
+    public Set<Animal> getAnimals() {
+        return animals;
+    }
+
+
 
 }
--- alpacashow-admin-backend/src/test/java/nl/animundo/apps/alpacashowadmin/backend/domain/ParticipantTest.java
+++ alpacashow-admin-backend/src/test/java/nl/animundo/apps/alpacashowadmin/backend/domain/ParticipantTest.java
@@ -4,6 +4,10 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.Assert.assertEquals;
 
 
@@ -15,10 +19,6 @@ public class ParticipantTest {
     @Rule
     public ExpectedException exception = ExpectedException.none();
 
-    /* TODO
-    Check unique participant
-     */
-
     @Test
     public void newParticipantWithTrim() {
 
@@ -27,8 +27,21 @@ public class ParticipantTest {
         Participant participant = new Participant(name);
 
         assertEquals("New Participant", participant.getName());
-
     }
+
+    @Test
+    public void newParticipantWithAnimals() {
+
+        String name = " New Participant";
+        Set<Animal> animals = new HashSet<>();
+        animals.add(new Animal("Alpaca1", BreedClass.HUACAYA, SexClass.FEMALE, ColorClass.BLACK, LocalDate.now().minusYears(1), "123456789", null, "Vader", "Moeder"));
+        animals.add(new Animal("Alpaca2", BreedClass.SURI, SexClass.MALE, ColorClass.FANCY, LocalDate.now().minusYears(2), "987654321", "BAF12345", "Vader2", "Moeder2"));
+
+        Participant participant = new Participant(name, animals);
+
+        assertEquals(animals, participant.getAnimals());
+    }
+
 
     @Test
     public void participantNotNull() {
@@ -40,5 +53,4 @@ public class ParticipantTest {
 
         new Participant(name);
     }
-
 }
