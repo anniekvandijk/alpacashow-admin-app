ff0d88f6ba69d38ea841e8dc162030ee22390bc8
Anniek van Dijk <anniek.vandijk@duo.nl>
2016-06-16 12:43:17 +0200

    CucumberIT

---
+++ alpacashow-admin-it/src/main/test/java/nl/animundo/apps/alpacashowadmin/it/RunCucumberIT.java
@@ -0,0 +1,18 @@
+package nl.animundo.apps.alpacashowadmin.it;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+/**
+ * Created by Anniek van Dijk on 16-6-2016.
+ */
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = { "html:target/cucumber/report.html", "json:target/cucumber-report.json" }
+        ,features = { "src/main/test/resources/features" }
+        //,tags = { "@run"}
+)
+public class RunCucumberIT {
+    //
+}
