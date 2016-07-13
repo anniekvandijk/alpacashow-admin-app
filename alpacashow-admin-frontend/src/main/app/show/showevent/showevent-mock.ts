139de4e3fc4046c49fc7881126312ff8ed5b2562
Anniek van Dijk <anniek@animundo.nl>
2016-07-13 15:45:57 +0200

    showevent mock data in table

--- alpacashow-admin-frontend/src/main/app/show/showevent/showevent-mock.ts
+++ alpacashow-admin-frontend/src/main/app/show/showevent/showevent-mock.ts
@@ -2,14 +2,8 @@ import { ShowEvent } from './showevent';
 
 
 export const SHOWEVENTS: ShowEvent[] = [
-    {name: 'Int. Alpacashow'  , location: 'Assen' },
-    {name: 'Narco'     , location: 'Beilen' },
-    {name: 'Bombasto'  , location: 'Assen' },
-    {name: 'Celeritas' , location: 'Assen' },
-    {name: 'Magneta'   , location: 'Assen' },
-    {name: 'RubberMan' , location: 'Assen' },
-    {name: 'Dynama'    , location: 'Assen' },
-    {name: 'Dr IQ'     , location: 'Assen' },
-    {name: 'Magma'     , location: 'Assen' },
-    {name: 'Tornado'   , location: 'Assen' }
+    {name: 'Int. Alpacashow'  , location: 'Assen'  , date: '01-02-2012' , closedate: '01-01-2012' , judge: 'judge1' , showtype: 'Haltershow'},
+    {name: 'Alpacashow Beilen', location: 'Beilen' , date: '01-03-2013' , closedate: '01-01-2013' , judge: 'judge2' , showtype: 'Fleeceshow, Haltershow'},
+    {name: 'Bombasto'         , location: 'Assen'  , date: '01-04-2014' , closedate: '01-01-2014' , judge: 'judge3' , showtype: 'Male progeny show'},
+
 ];
--- alpacashow-admin-frontend/src/main/app/show/showevent/showevent.ts
+++ alpacashow-admin-frontend/src/main/app/show/showevent/showevent.ts
@@ -1,4 +1,8 @@
 export class ShowEvent {
     name: string;
     location: string;
+    date: string;
+    closedate: string;
+    judge: string;
+    showtype: string;
 }
