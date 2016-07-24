17d65c2a8a67f07b8eec9657360de95440148a7b
Anniek van Dijk <anniek@animundo.nl>
2016-07-24 15:22:30 +0200

    Add test for not finding a show with search

--- alpacashow-admin-backend/src/test/java/nl/animundo/apps/alpacashowadmin/backend/repositories/ShowEventRepositoryTest.java
+++ alpacashow-admin-backend/src/test/java/nl/animundo/apps/alpacashowadmin/backend/repositories/ShowEventRepositoryTest.java
@@ -43,6 +43,33 @@ public class ShowEventRepositoryTest {
         assertEquals("Test showEvent", showEventRepository.search(searchForKey).getName());
 
     }
+
+    @Test
+    public void searchForNotKnownShow() {
+
+        exception.expect(NullPointerException.class);
+
+        String name = "Test showEvent";
+        LocalDate date = LocalDate.of(2017, 6, 15);
+        LocalDate closeDate = LocalDate.of(2017, 4, 15);
+        String location = "Surhuisterveen";
+        String judge = " Test Judge ";
+        Set<Show> shows = new HashSet<>();
+
+        shows.add(new Show(ShowType.FLEECESHOW));
+        shows.add(new Show(ShowType.HALTERSHOW));
+        shows.add(new Show(ShowType.FEMALE_PROGENY_SHOW));
+        shows.add(new Show(ShowType.MALE_PROGENY_SHOW));
+
+        ShowEvent showEvent = new ShowEvent(name, date, closeDate, location, judge, shows);
+        ShowEventRepository showEventRepository = new ShowEventRepository();
+
+        showEventRepository.add(showEvent);
+
+        String searchForKey = "Test showEvent_2017-06-14";
+        showEventRepository.search(searchForKey).getName();
+
+    }
 }
 
 
