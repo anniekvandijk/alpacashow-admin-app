9218a115d6b89d12cf452f187cac4629221d5221
Anniek van Dijk <anniek@animundo.nl>
2016-08-07 20:05:34 +0200

    Controller now loads application properties and repository. But wrong path :(

--- alpacashow-admin-backend/src/main/java/nl/animundo/apps/alpacashowadmin/backend/controllers/ShowEventController.java
+++ alpacashow-admin-backend/src/main/java/nl/animundo/apps/alpacashowadmin/backend/controllers/ShowEventController.java
@@ -2,10 +2,11 @@ package nl.animundo.apps.alpacashowadmin.backend.controllers;
 
 import nl.animundo.apps.alpacashowadmin.backend.domain.ShowEvent;
 import nl.animundo.apps.alpacashowadmin.backend.repositories.ShowEventRepository;
-import nl.animundo.apps.alpacashowadmin.backend.repositories.csv.CsvShowEventRepository;
+import nl.animundo.apps.alpacashowadmin.backend.services.application.ApplicationRepositoryService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.util.Collection;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -22,17 +23,22 @@ import javax.ws.rs.core.MediaType;
 @Path("/showevent")
 public class ShowEventController {
     private static Logger logger = LoggerFactory.getLogger(ShowEventController.class);
+    private ShowEventRepository showEventRepo;
+    private String environment = "dev";
 
-// /alpacashow-admin-app/backend/webservice/showevent/
 
-    ShowEventRepository showEvent = new CsvShowEventRepository();
+    private void loadRepository() throws IOException {
+
+        showEventRepo = ApplicationRepositoryService.getShowEventRepository(environment);
+    }
+
+    // /alpacashow-admin-app/backend/webservice/showevent/
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Collection<ShowEvent> getShowEvents()
-    {
-
-        Collection<ShowEvent> listOfShowEvents=showEvent.getAllShowEvents();
+    public Collection<ShowEvent> getShowEvents() throws IOException {
+        loadRepository();
+        Collection<ShowEvent> listOfShowEvents=showEventRepo.getAllShowEvents();
         logger.info("Getting showevents");
         return listOfShowEvents;
     }
@@ -40,24 +46,24 @@ public class ShowEventController {
     @GET
     @Path("/{key}")
     @Produces(MediaType.APPLICATION_JSON)
-    public ShowEvent getShowEventByKey(@PathParam("key") String key)
-    {
-        return showEvent.getShowEventsByKeySet(key);
+    public ShowEvent getShowEventByKey(@PathParam("key") String key) throws IOException {
+        loadRepository();
+        return showEventRepo.getShowEventsByKeySet(key);
     }
 
     @POST
     @Produces(MediaType.APPLICATION_JSON)
-    public void addShowEvent(ShowEvent showEvent)
-    {
-        this.showEvent.add(showEvent);
+    public void addShowEvent(ShowEvent showEvent) throws IOException {
+        loadRepository();
+        this.showEventRepo.add(showEvent);
     }
 
     @DELETE
     @Path("/{key}")
     @Produces(MediaType.APPLICATION_JSON)
-    public void deleteShowEvent(@PathParam("key") String key)
-    {
-        this.showEvent.delete(key);
+    public void deleteShowEvent(@PathParam("key") String key) throws IOException {
+        loadRepository();
+        this.showEventRepo.delete(key);
 
     }
 }
