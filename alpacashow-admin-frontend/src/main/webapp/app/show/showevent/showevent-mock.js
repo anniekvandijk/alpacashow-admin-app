f0a7d3da52d60aceb34811a014377fd26d311ae9
Anniek van Dijk <anniek@animundo.nl>
2016-08-28 20:44:54 +0200

    update frondend to Angular 2 RC-5

--- alpacashow-admin-frontend/src/main/webapp/app/show/showevent/showevent-mock.js
+++ alpacashow-admin-frontend/src/main/webapp/app/show/showevent/showevent-mock.js
@@ -1,6 +1,6 @@
 "use strict";
 exports.SHOWEVENTS = [
-    { name: 'Int. Alpacashow', location: 'Assen', date: '01-02-2012', closedate: '01-01-2012', judge: 'judge1', showtype: 'Haltershow' },
+    { name: 'Int Alpacashow', location: 'Assen', date: '01-02-2012', closedate: '01-01-2012', judge: 'judge1', showtype: 'Haltershow' },
     { name: 'Alpacashow Beilen', location: 'Beilen', date: '01-03-2013', closedate: '01-01-2013', judge: 'judge2', showtype: 'Fleeceshow, Haltershow' },
     { name: 'Bombasto', location: 'Assen', date: '01-04-2014', closedate: '01-01-2014', judge: 'judge3', showtype: 'Male progeny show' },
 ];
--- alpacashow-admin-frontend/src/main/webapp/app/show/showevent/showevent-mock.ts
+++ alpacashow-admin-frontend/src/main/webapp/app/show/showevent/showevent-mock.ts
@@ -2,7 +2,7 @@ import { ShowEvent } from './showevent';
 
 
 export const SHOWEVENTS: ShowEvent[] = [
-    {name: 'Int. Alpacashow'  , location: 'Assen'  , date: '01-02-2012' , closedate: '01-01-2012' , judge: 'judge1' , showtype: 'Haltershow'},
+    {name: 'Int Alpacashow'  , location: 'Assen'  , date: '01-02-2012' , closedate: '01-01-2012' , judge: 'judge1' , showtype: 'Haltershow'},
     {name: 'Alpacashow Beilen', location: 'Beilen' , date: '01-03-2013' , closedate: '01-01-2013' , judge: 'judge2' , showtype: 'Fleeceshow, Haltershow'},
     {name: 'Bombasto'         , location: 'Assen'  , date: '01-04-2014' , closedate: '01-01-2014' , judge: 'judge3' , showtype: 'Male progeny show'},
 
