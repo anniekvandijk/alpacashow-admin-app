d2c1a32e126263651fff69dc3b7642295cb4e37a
Anniek van Dijk <anniek@animundo.nl>
2017-01-13 10:02:00 +0100

    add enumservice

---
+++ alpacashow-admin-frontend/src/main/webapp/app/Services/enumservices.js
@@ -0,0 +1,8 @@
+"use strict";
+var EnumServices = (function () {
+    function EnumServices() {
+    }
+    return EnumServices;
+}());
+exports.EnumServices = EnumServices;
+//# sourceMappingURL=enumservices.js.map
