1903b8b391187b6186012f01b1aeac384edacd3d
Anniek van Dijk <anniek@animundo.nl>
2017-01-19 23:20:18 +0100

    add FleeceWeightPoints Repository

---
+++ alpacashow-admin-backend/src/main/java/nl/animundo/apps/alpacashowadmin/backend/repositories/FleeceWeightPointsRepository.java
@@ -0,0 +1,56 @@
+package nl.animundo.apps.alpacashowadmin.backend.repositories;
+
+import nl.animundo.apps.alpacashowadmin.backend.helpclasses.FleeceWeightPoints;
+import nl.animundo.apps.alpacashowadmin.backend.utilities.FleeceWeightPointsComparator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.*;
+
+public class FleeceWeightPointsRepository {
+    private static Logger logger = LoggerFactory.getLogger(FleeceWeightPointsRepository.class);
+    private Map<String, FleeceWeightPoints> fleeceWeightPointsMap = new HashMap<>();
+
+    public String add(final FleeceWeightPoints fleeceWeightPoints) {
+
+        String fleeceWeightPointsKey = fleeceWeightPoints.getBreed() + "_" + fleeceWeightPoints.getAgeClass() + "_" + fleeceWeightPoints.getCleanFleeceWeight();
+        if (getFleeceWeightPointsByKeySet(fleeceWeightPointsKey) != null) {
+            fleeceWeightPointsMap.remove(fleeceWeightPointsKey);
+            fleeceWeightPointsMap.put(fleeceWeightPointsKey, fleeceWeightPoints);
+            logger.info("Updated fleeceWeightPoints '" + fleeceWeightPointsKey + "' to fleeceWeightPointsRepo");
+        } else {
+            fleeceWeightPointsMap.put(fleeceWeightPointsKey, fleeceWeightPoints);
+            logger.info("Added fleeceWeightPoints '" + fleeceWeightPointsKey + "' to fleeceWeightPointsRepo");
+        }
+        return fleeceWeightPointsKey;
+    }
+
+    public String delete(final String fleeceWeightPointsKey) {
+
+        FleeceWeightPoints fleeceWeightPointsToDelete = getFleeceWeightPointsByKeySet(fleeceWeightPointsKey);
+        if (fleeceWeightPointsToDelete != null) {
+            fleeceWeightPointsMap.remove(fleeceWeightPointsKey);
+            logger.info("Deleted showEventAnimal '" + fleeceWeightPointsKey + "' from showEventAnimalRepo");
+            return fleeceWeightPointsToDelete.getBreed() + "_" + fleeceWeightPointsToDelete.getAgeClass() + "_" + fleeceWeightPointsToDelete.getCleanFleeceWeight();
+        } else {
+            return null;
+        }
+    }
+
+    public Set<String> getAllFleeceWeightPointsByKeySet() {
+        return fleeceWeightPointsMap.keySet();
+    }
+
+    public SortedSet<FleeceWeightPoints> getAllFleeceWeightPoints() {
+        SortedSet<FleeceWeightPoints> fleeceWeightPointssSet = new TreeSet<>(new FleeceWeightPointsComparator());
+        for (FleeceWeightPoints points : fleeceWeightPointsMap.values()) {
+            fleeceWeightPointssSet.add(points);
+        }
+        return fleeceWeightPointssSet;
+    }
+
+    public FleeceWeightPoints getFleeceWeightPointsByKeySet(final String keySet) {
+        return fleeceWeightPointsMap.get(keySet);
+    }
+
+}
