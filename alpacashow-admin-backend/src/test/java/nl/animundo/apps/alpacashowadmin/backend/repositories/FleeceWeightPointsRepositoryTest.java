f2f6f99521775b8752792df1a8f8bdcff00c9767
Anniek van Dijk <anniek@animundo.nl>
2017-01-20 01:19:24 +0100

    add FleeceWeightPointsRepository and CsvFleeceWeightPointsRepository tests

---
+++ alpacashow-admin-backend/src/test/java/nl/animundo/apps/alpacashowadmin/backend/repositories/FleeceWeightPointsRepositoryTest.java
@@ -0,0 +1,186 @@
+package nl.animundo.apps.alpacashowadmin.backend.repositories;
+
+import nl.animundo.apps.alpacashowadmin.backend.domain.ShowEventAnimal;
+import nl.animundo.apps.alpacashowadmin.backend.domain.enums.AgeClass;
+import nl.animundo.apps.alpacashowadmin.backend.domain.enums.BreedClass;
+import nl.animundo.apps.alpacashowadmin.backend.helpclasses.FleeceWeightPoints;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.LocalDate;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class FleeceWeightPointsRepositoryTest {
+    private static Logger logger = LoggerFactory.getLogger(FleeceWeightPointsRepositoryTest.class);
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    @Test
+    public void addFleeceWeightPoints() {
+
+        BreedClass breed          = BreedClass.HUACAYA_FLEECE;
+        AgeClass ageClass         = AgeClass.JUNIOR;
+        float cleanFleeceWeight   = 1.5f;
+        float weightPoints        = 9.0f;
+
+        FleeceWeightPoints fleeceWeightPoints = new FleeceWeightPoints(breed, ageClass,cleanFleeceWeight,weightPoints);
+
+        FleeceWeightPointsRepository fleeceWeightPointsRepository = new FleeceWeightPointsRepository();
+
+        assertEquals(0, fleeceWeightPointsRepository.getAllFleeceWeightPoints().size());
+
+        fleeceWeightPointsRepository.add(fleeceWeightPoints);
+        assertEquals(1, fleeceWeightPointsRepository.getAllFleeceWeightPoints().size());
+
+        String key = "HUACAYA_FLEECE_JUNIOR_1.5";
+
+        assertEquals(breed, fleeceWeightPointsRepository.getFleeceWeightPointsByKeySet(key).getBreed());
+        assertEquals(ageClass, fleeceWeightPointsRepository.getFleeceWeightPointsByKeySet(key).getAgeClass());
+        assertEquals(cleanFleeceWeight, fleeceWeightPointsRepository.getFleeceWeightPointsByKeySet(key).getCleanFleeceWeight(), 0.05);
+        assertEquals(weightPoints, fleeceWeightPointsRepository.getFleeceWeightPointsByKeySet(key).getWeightPoints(), 0.05);
+    }
+
+    @Test
+    public void addFleeceWeightPointsWithSameKey() {
+
+        BreedClass breed          = BreedClass.HUACAYA_FLEECE;
+        AgeClass ageClass         = AgeClass.JUNIOR;
+        float cleanFleeceWeight   = 1.5f;
+        float weightPoints1        = 9.0f;
+        float weightPoints2        = 7.0f;
+
+        FleeceWeightPoints fleeceWeightPoints1 = new FleeceWeightPoints(breed, ageClass,cleanFleeceWeight,weightPoints1);
+        FleeceWeightPoints fleeceWeightPoints2 = new FleeceWeightPoints(breed, ageClass,cleanFleeceWeight,weightPoints2);
+
+        FleeceWeightPointsRepository fleeceWeightPointsRepository = new FleeceWeightPointsRepository();
+
+        assertEquals(0, fleeceWeightPointsRepository.getAllFleeceWeightPoints().size());
+
+        fleeceWeightPointsRepository.add(fleeceWeightPoints1);
+        fleeceWeightPointsRepository.add(fleeceWeightPoints2);
+
+        assertEquals(1, fleeceWeightPointsRepository.getAllFleeceWeightPoints().size());
+
+        String key = "HUACAYA_FLEECE_JUNIOR_1.5";
+
+        assertEquals(breed, fleeceWeightPointsRepository.getFleeceWeightPointsByKeySet(key).getBreed());
+        assertEquals(ageClass, fleeceWeightPointsRepository.getFleeceWeightPointsByKeySet(key).getAgeClass());
+        assertEquals(cleanFleeceWeight, fleeceWeightPointsRepository.getFleeceWeightPointsByKeySet(key).getCleanFleeceWeight(), 0.05);
+        assertEquals(weightPoints2, fleeceWeightPointsRepository.getFleeceWeightPointsByKeySet(key).getWeightPoints(), 0.05);
+
+    }
+
+    @Test
+    public void deleteFleeceWeightPoints() {
+
+        BreedClass breed          = BreedClass.HUACAYA_FLEECE;
+        AgeClass ageClass         = AgeClass.JUNIOR;
+        float cleanFleeceWeight   = 1.5f;
+        float weightPoints1        = 9.0f;
+        float weightPoints2        = 7.0f;
+
+        FleeceWeightPoints fleeceWeightPoints1 = new FleeceWeightPoints(breed, ageClass,cleanFleeceWeight,weightPoints1);
+        FleeceWeightPoints fleeceWeightPoints2 = new FleeceWeightPoints(breed, ageClass,cleanFleeceWeight,weightPoints2);
+
+        FleeceWeightPointsRepository fleeceWeightPointsRepository = new FleeceWeightPointsRepository();
+
+        assertEquals(0, fleeceWeightPointsRepository.getAllFleeceWeightPoints().size());
+
+        fleeceWeightPointsRepository.add(fleeceWeightPoints1);
+        fleeceWeightPointsRepository.add(fleeceWeightPoints2);
+
+        assertEquals(1, fleeceWeightPointsRepository.getAllFleeceWeightPoints().size());
+
+        String key = "HUACAYA_FLEECE_JUNIOR_1.5";
+
+        fleeceWeightPointsRepository.delete(key);
+
+        assertEquals(0, fleeceWeightPointsRepository.getAllFleeceWeightPoints().size());
+
+    }
+
+    @Test
+    public void deleteFleeceWeightPointsWithNotKnownKey() {
+
+        exception.expect(NullPointerException.class);
+
+        BreedClass breed          = BreedClass.HUACAYA_FLEECE;
+        AgeClass ageClass         = AgeClass.JUNIOR;
+        float cleanFleeceWeight   = 1.5f;
+        float weightPoints        = 9.0f;
+
+        FleeceWeightPoints fleeceWeightPoints = new FleeceWeightPoints(breed, ageClass,cleanFleeceWeight,weightPoints);
+
+        FleeceWeightPointsRepository fleeceWeightPointsRepository = new FleeceWeightPointsRepository();
+
+        assertEquals(0, fleeceWeightPointsRepository.getAllFleeceWeightPoints().size());
+
+        fleeceWeightPointsRepository.add(fleeceWeightPoints);
+        assertEquals(1, fleeceWeightPointsRepository.getAllFleeceWeightPoints().size());
+
+        String key = "HUACAYA_FLEECE_JUNIOR_1.54";
+
+        fleeceWeightPointsRepository.delete(key);
+
+        assertEquals(1, fleeceWeightPointsRepository.getAllFleeceWeightPoints().size());
+        assertEquals(null, fleeceWeightPointsRepository.delete(key).toString());
+    }
+
+    @Test
+    public void getNotKnownFleeceWeightPoints() {
+
+        exception.expect(NullPointerException.class);
+
+        BreedClass breed          = BreedClass.HUACAYA_FLEECE;
+        AgeClass ageClass         = AgeClass.JUNIOR;
+        float cleanFleeceWeight   = 1.5f;
+        float weightPoints        = 9.0f;
+
+        FleeceWeightPoints fleeceWeightPoints = new FleeceWeightPoints(breed, ageClass,cleanFleeceWeight,weightPoints);
+
+        FleeceWeightPointsRepository fleeceWeightPointsRepository = new FleeceWeightPointsRepository();
+
+        assertEquals(0, fleeceWeightPointsRepository.getAllFleeceWeightPoints().size());
+
+        fleeceWeightPointsRepository.add(fleeceWeightPoints);
+        assertEquals(1, fleeceWeightPointsRepository.getAllFleeceWeightPoints().size());
+
+        String key = "HUACAYA_FLEECE_JUNIOR_1.54";
+
+        fleeceWeightPointsRepository.getFleeceWeightPointsByKeySet(key).getBreed();
+
+    }
+
+    @Test
+    public void getAllFleeceWeightPointsByKeySet() {
+
+        BreedClass breed1          = BreedClass.HUACAYA_FLEECE;
+        BreedClass breed2          = BreedClass.SURI_FLEECE;
+        AgeClass ageClass         = AgeClass.JUNIOR;
+        float cleanFleeceWeight   = 1.5f;
+        float weightPoints        = 9.0f;
+
+        FleeceWeightPoints fleeceWeightPoints1 = new FleeceWeightPoints(breed1, ageClass,cleanFleeceWeight,weightPoints);
+        FleeceWeightPoints fleeceWeightPoints2 = new FleeceWeightPoints(breed2, ageClass,cleanFleeceWeight,weightPoints);
+
+        FleeceWeightPointsRepository fleeceWeightPointsRepository = new FleeceWeightPointsRepository();
+
+        assertEquals(0, fleeceWeightPointsRepository.getAllFleeceWeightPoints().size());
+
+        fleeceWeightPointsRepository.add(fleeceWeightPoints1);
+        fleeceWeightPointsRepository.add(fleeceWeightPoints2);
+
+        assertEquals(2, fleeceWeightPointsRepository.getAllFleeceWeightPoints().size());
+        assertTrue(fleeceWeightPointsRepository.getAllFleeceWeightPointsByKeySet().contains("HUACAYA_FLEECE_JUNIOR_1.5"));
+        assertTrue(fleeceWeightPointsRepository.getAllFleeceWeightPointsByKeySet().contains("SURI_FLEECE_JUNIOR_1.5"));
+
+    }
+}
+
+
---
+++ alpacashow-admin-backend/src/test/java/nl/animundo/apps/alpacashowadmin/backend/repositories/csv/CsvFleeceWeightPoinslRepositoryTest.java
@@ -0,0 +1,40 @@
+package nl.animundo.apps.alpacashowadmin.backend.repositories.csv;
+
+import nl.animundo.apps.alpacashowadmin.backend.helpclasses.FleeceWeightPoints;
+import nl.animundo.apps.alpacashowadmin.backend.repositories.FleeceWeightPointsRepository;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import java.io.*;
+import static org.junit.Assert.*;
+
+public class CsvFleeceWeightPoinslRepositoryTest {
+
+    final String workingDir = System.getProperty("user.dir");
+    final String testFileDir = "/src/test/resources/csv/helpfiles/";
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    @Test
+    public void importFile() throws IOException {
+
+        File importFile = new File(workingDir + testFileDir + "FLEECEWEIGHTPOINTS.csv");
+
+        assertTrue(importFile.isFile() && importFile.exists() && importFile.canRead());
+        Reader reader = new FileReader(importFile);
+
+        FleeceWeightPointsRepository repo = CsvFleeceWeightPointsRepository.importData(reader);
+        reader.close();
+
+        String key = "HUACAYA_MATURE_5.0";
+        FleeceWeightPoints fleeceWeightPoints = repo.getFleeceWeightPointsByKeySet(key);
+        assertNotNull(fleeceWeightPoints);
+        assertEquals("HUACAYA", fleeceWeightPoints.getBreed().toString());
+        assertEquals("MATURE", fleeceWeightPoints.getAgeClass().toString());
+        assertEquals(5.0f, fleeceWeightPoints.getCleanFleeceWeight(), 0.05);
+        assertEquals(15.0f, fleeceWeightPoints.getWeightPoints(), 0.05);
+    }
+}
+
+
